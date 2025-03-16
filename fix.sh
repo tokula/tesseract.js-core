3be1d0d111fe17987246d00ba9d9b4b6406cb3e9
Torben Kuhlmann Forsström <8672626+tokula@users.noreply.github.com>
2025-03-16 22:24:54 +0100

    line endings wip

---
+++ fix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eo pipefail
+
+SCRIPT_ROOT=$(dirname $0)/third_party
+
+#!/bin/bash
+echo "start 1234"
+#find $SCRIPT_ROOT -type f -print0 | xargs -0 dos2unix
+#find $SCRIPT_ROOT -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -type f -print0 | xargs -0 dos2unix
+find $SCRIPT_ROOT -regex ".*\.\(sh\|sc\)" -type f -print0 | xargs -0 dos2unix
+
+echo "end"
