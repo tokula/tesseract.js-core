61278aac5746aada75426fac9b8bd2264f126dcd
Torben Kuhlmann Forsström <8672626+tokula@users.noreply.github.com>
2025-03-19 15:33:44 +0100

    wip

--- fix.sh
+++ fix.sh
@@ -5,9 +5,9 @@ set -eo pipefail
 SCRIPT_ROOT=$(dirname $0)/third_party
 
 #!/bin/bash
-echo "start 1234"
+echo "start"
 #find $SCRIPT_ROOT -type f -print0 | xargs -0 dos2unix
 #find $SCRIPT_ROOT -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -type f -print0 | xargs -0 dos2unix
-find $SCRIPT_ROOT -regex ".*\.\(sh\|sc\)" -type f -print0 | xargs -0 dos2unix
+find $SCRIPT_ROOT -regex ".*\.\(sh\|ac\)" -type f -print0 | xargs -0 dos2unix
 
 echo "end"
